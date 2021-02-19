04fe95643eb7ce9828be14ba946f61451a2eeee1
nabeeltaariq <nabeeltaariq@gmail.com>
2021-02-19 07:47:11 -0800

    merging commit

--- app/Questionaire.php
+++ app/Questionaire.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Questionaire extends Model
 {
     protected $guarded = [];
+    public function user()
+    {
+        return $this->belongsTo(\App\User::class);
+    }
 }
