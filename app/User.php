fe28a4918d409e1272298cfaeea91beb5224d3ae
nabeeltaariq <nabeeltaariq@gmail.com>
2021-02-19 08:12:19 -0800

    merge

--- app/User.php
+++ app/User.php
@@ -36,5 +36,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-<
+
+
+
+
+
+    public function questionaires()
+    {
+        return $this->hasMany(\App\Questionaire::class);
+    }
 }
